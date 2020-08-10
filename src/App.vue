0b54734a7a7de1b9ea46edd3759f5fc5d4de0452
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-10 03:25:38 -0400

    Remove Vue hello-world, barebones project setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav>Pretend there's a nav-bar here.</nav>
+    <router-view />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
