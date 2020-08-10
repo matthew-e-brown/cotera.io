4e7da6c2ecbbad7ecae6b670f4e33bbbee5b0d0b
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-10 04:35:22 -0400

    Items and armour schema up and running
    - Renamed and removed some assets to fit better

---
+++ src/armour.js
@@ -0,0 +1,463 @@
+export default armour = [
+  { // ---------------------------------------- Hylian set
+    'set': 'Hylian Set',
+    'head': {
+      'name': 'Hylian Hood',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Hylian Tunic',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Hylian Trousers',
+      'upgrades': [
+        [],
+        [],
+        [],
+      ]
+    }
+  },
+  { // ---------------------------------------- Set of the Wild
+    'set': 'Hero of the Wild Set',
+    'head': {
+      'name': 'Cap of the Wild',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Tunic of the Wild',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Trousers of the Wild',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Zora armour
+    'set': 'Zora Set',
+    'head': {
+      'name': 'Zora Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Zora Armour',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Zora Greaves',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Desert voe armour
+    'set': 'Desert Voe Set',
+    'head': {
+      'name': 'Desert Voe Headband',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Desert Voe Spaulder',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Desert Voe Trousers',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Snowquill set
+    'set': 'Snowquill Set',
+    'head': {
+      'name': 'Snowquill Headdress',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Snowquill Tunic',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Snowquill Trowsers',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Flamebreaker armour
+    'set': 'Flamebreaker Set',
+    'head': {
+      'name': 'Flamebreaker Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Flamebreaker Armour',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Flamebreaker Boots',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Stealth set
+    'set': 'Stealth Set',
+    'head': {
+      'name': 'Stealth Mash',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Stealth Chest Guard',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Stealth Tights',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Climbing gear
+    'set': 'Climbing Set',
+    'head': {
+      'name': 'Climber\'s Bandanna',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Climbing Gear',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Climbing Boots',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Radiant outfit
+    'set': 'Radiant Set',
+    'head': {
+      'name': 'Radiant Mask',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Radiant Shirt',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Radiant Tights',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Knight's armour
+    'set': 'Soldier\'s Set',
+    'head': {
+      'name': 'Soldier\'s Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Soldier\'s Armour',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Soldier\'s Greaves',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Ancient armour
+    'set': 'Ancient Set',
+    'head': {
+      'name': 'Ancient Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Ancient Cuirass',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Ancient Greaves',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Rubber armour
+    'set': 'Rubber Set',
+    'head': {
+      'name': 'Rubber Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Rubber Armour',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Rubber Tights',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Barbarian armour
+    'set': 'Barbarian Set',
+    'head': {
+      'name': 'Barbarian Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'torso': {
+      'name': 'Barbarian Armour',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    },
+    'legs': {
+      'name': 'Barbarian Leg Wraps',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Champion's Tunic
+    'torso': {
+      'name': '',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Diamond Circlet
+    'head': {
+      'name': 'Champion\'s Tunic',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Ruby Circlet
+    'head': {
+      'name': 'Ruby Circlet',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Sapphire Circlet
+    'head': {
+      'name': 'Sapphire Circlet',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Topaz Earrings
+    'head': {
+      'name': 'Topaz Earrings',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Opal Earrings
+    'head': {
+      'name': 'Opal Earrings',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Amber Earrings
+    'head': {
+      'name': 'Amber Earrings',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Vah Ruta Divine Helm
+    'head': {
+      'name': 'Vah Ruta Divine Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Vah Medoh Divine Helm
+    'head': {
+      'name': 'Vah Medoh Divine Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Vah Rudania Divine Helm
+    'head': {
+      'name': 'Vah Rudania Divine Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  },
+  { // ---------------------------------------- Vah Naboris Divine Helm
+    'head': {
+      'name': 'Vah Naboris Divine Helm',
+      'upgrades': [
+        [],
+        [],
+        []
+      ]
+    }
+  }
+];
