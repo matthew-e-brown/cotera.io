2046a8bad2b172f6bb730b30c05f322edc01f022
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-11 02:15:47 -0400

    Flatten data structure
    - Add IDs to make developing and finding sprites easier
    - Separate sets and items
    - Rename 'torso' to 'body' for all-4-letter matching!

--- src/armor.js
+++ src/armor.js
@@ -1,513 +1,405 @@
 export default armor = [
-  { // ---------------------------------------- Hylian set
-    'set': 'Hylian Set',
-    'head': {
-      'name': 'Hylian Hood',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Hylian Tunic',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Hylian Trousers',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Hylian set
+  {
+    'id': 'head_0',
+    'set': 'set_0',
+    'name': 'Hylian Hood',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Set of the Wild
-    'set': 'Hero of the Wild Set',
-    'head': {
-      'name': 'Cap of the Wild',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Tunic of the Wild',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Trousers of the Wild',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_0',
+    'name': 'Hylian Tunic',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Zora armor
-    'set': 'Zora Set',
-    'head': {
-      'name': 'Zora Helm',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Zora Armor',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Zora Greaves',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_0',
+    'name': 'Hylian Trousers',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Desert voe armor
-    'set': 'Desert Voe Set',
-    'head': {
-      'name': 'Desert Voe Headband',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Desert Voe Spaulder',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Desert Voe Trousers',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Set of the Wild
+  {
+    'id': 'head_1',
+    'name': 'Cap of the Wild',
+    'base': 4,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Snowquill set
-    'set': 'Snowquill Set',
-    'head': {
-      'name': 'Snowquill Headdress',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Snowquill Tunic',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Snowquill Trousers',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_1',
+    'name': 'Tunic of the Wild',
+    'base': 4,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Flamebreaker armor
-    'set': 'Flamebreaker Set',
-    'head': {
-      'name': 'Flamebreaker Helm',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Flamebreaker Armor',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Flamebreaker Boots',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_1',
+    'name': 'Trousers of the Wild',
+    'base': 4,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Stealth set
-    'set': 'Stealth Set',
-    'head': {
-      'name': 'Stealth Mash',
-      'base': 2,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Stealth Chest Guard',
-      'base': 2,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Stealth Tights',
-      'base': 2,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Zora armor
+  {
+    'id': 'head_2',
+    'name': 'Zora Helm',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Climbing gear
-    'set': 'Climbing Set',
-    'head': {
-      'name': 'Climber\'s Bandanna',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Climbing Gear',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Climbing Boots',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_2',
+    'name': 'Zora Armor',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Radiant outfit
-    'set': 'Radiant Set',
-    'head': {
-      'name': 'Radiant Mask',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Radiant Shirt',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Radiant Tights',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_2',
+    'name': 'Zora Greaves',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Knight's armor
-    'set': 'Soldier\'s Set',
-    'head': {
-      'name': 'Soldier\'s Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Soldier\'s Armor',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Soldier\'s Greaves',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Desert voe armor
+  {
+    'id': 'head_3',
+    'name': 'Desert Voe Headband',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Ancient armor
-    'set': 'Ancient Set',
-    'head': {
-      'name': 'Ancient Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Ancient Cuirass',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Ancient Greaves',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_3',
+    'name': 'Desert Voe Spaulder',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Rubber armor
-    'set': 'Rubber Set',
-    'head': {
-      'name': 'Rubber Helm',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Rubber Armor',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Rubber Tights',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_3',
+    'name': 'Desert Voe Trousers',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Barbarian armor
-    'set': 'Barbarian Set',
-    'head': {
-      'name': 'Barbarian Helm',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'torso': {
-      'name': 'Barbarian Armor',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    },
-    'legs': {
-      'name': 'Barbarian Leg Wraps',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Snowquill set
+  {
+    'id': 'head_4',
+    'name': 'Snowquill Headdress',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Champion's Tunic
-    'torso': {
-      'name': 'Champion\'s Tunic',
-      'base': 5,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_4',
+    'name': 'Snowquill Tunic',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Diamond Circlet
-    'head': {
-      'name': 'Diamond Circlet',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_4',
+    'name': 'Snowquill Trousers',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Ruby Circlet
-    'head': {
-      'name': 'Ruby Circlet',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Flamebreaker armor
+  {
+    'id': 'head_5',
+    'name': 'Flamebreaker Helm',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Sapphire Circlet
-    'head': {
-      'name': 'Sapphire Circlet',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_5',
+    'name': 'Flamebreaker Armor',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Topaz Earrings
-    'head': {
-      'name': 'Topaz Earrings',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_5',
+    'name': 'Flamebreaker Boots',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Opal Earrings
-    'head': {
-      'name': 'Opal Earrings',
-      'base': 3,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Stealth set
+  {
+    'id': 'head_6',
+    'name': 'Stealth Mash',
+    'base': 2,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Amber Earrings
-    'head': {
-      'name': 'Amber Earrings',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_6',
+    'name': 'Stealth Chest Guard',
+    'base': 2,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Vah Ruta Divine Helm
-    'head': {
-      'name': 'Vah Ruta Divine Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_6',
+    'name': 'Stealth Tights',
+    'base': 2,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Vah Medoh Divine Helm
-    'head': {
-      'name': 'Vah Medoh Divine Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  // ---------------------------------------- Climbing gear
+  {
+    'id': 'head_7',
+    'name': 'Climber\'s Bandanna',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Vah Rudania Divine Helm
-    'head': {
-      'name': 'Vah Rudania Divine Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'body_7',
+    'name': 'Climbing Gear',
+    'base': 3,
+    'upgrades': undefined
   },
-  { // ---------------------------------------- Vah Naboris Divine Helm
-    'head': {
-      'name': 'Vah Naboris Divine Helm',
-      'base': 4,
-      'upgrades': [
-        [],
-        [],
-        []
-      ]
-    }
+  {
+    'id': 'legs_7',
+    'name': 'Climbing Boots',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Radiant outfit
+  {
+    'id': 'head_8',
+    'name': 'Radiant Mask',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_8',
+    'name': 'Radiant Shirt',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'legs_8',
+    'name': 'Radiant Tights',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Knight's armor
+  {
+    'id': 'head_9',
+    'name': 'Soldier\'s Helm',
+    'base': 4,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_9',
+    'name': 'Soldier\'s Armor',
+    'base': 4,
+    'upgrades': undefined
+  },
+  {
+    'id': 'legs_9',
+    'name': 'Soldier\'s Greaves',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Ancient armor
+  {
+    'id': 'head_10',
+    'name': 'Ancient Helm',
+    'base': 4,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_10',
+    'name': 'Ancient Cuirass',
+    'base': 4,
+    'upgrades': undefined
+  },
+  {
+    'id': 'legs_10',
+    'name': 'Ancient Greaves',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Rubber armor
+  {
+    'id': 'head_11',
+    'name': 'Rubber Helm',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_11',
+    'name': 'Rubber Armor',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'legs_11',
+    'name': 'Rubber Tights',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Barbarian armor
+  {
+    'id': 'head_12',
+    'name': 'Barbarian Helm',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_12',
+    'name': 'Barbarian Armor',
+    'base': 3,
+    'upgrades': undefined
+  },
+  {
+    'id': 'body_12',
+    'name': 'Barbarian Leg Wraps',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Champion's Tunic
+  {
+    'id': 'body_13',
+    'name': 'Champion\'s Tunic',
+    'base': 5,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Diamond Circlet
+  {
+    'id': 'head_13',
+    'name': 'Diamond Circlet',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Ruby Circlet
+  {
+    'id': 'head_14',
+    'name': 'Ruby Circlet',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Sapphire Circlet
+  {
+    'id': 'head_15',
+    'name': 'Sapphire Circlet',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Topaz Earrings
+  {
+    'id': 'head_16',
+    'name': 'Topaz Earrings',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Opal Earrings
+  {
+    'id': 'head_17',
+    'name': 'Opal Earrings',
+    'base': 3,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Amber Earrings
+  {
+    'id': 'head_18',
+    'name': 'Amber Earrings',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Vah Ruta Divine Helm
+  {
+    'id': 'head_19',
+    'name': 'Vah Ruta Divine Helm',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Vah Medoh Divine Helm
+  {
+    'id': 'head_20',
+    'name': 'Vah Medoh Divine Helm',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Vah Rudania Divine Helm
+  {
+    'id': 'head_21',
+    'name': 'Vah Rudania Divine Helm',
+    'base': 4,
+    'upgrades': undefined
+  },
+  // ---------------------------------------- Vah Naboris Divine Helm
+  {
+    'id': 'head_22',
+    'name': 'Vah Naboris Divine Helm',
+    'base': 4,
+    'upgrades': undefined
   }
 ];
+
+export const sets = [
+  {
+    'id': 'set_0',
+    'name': 'Hylian Set',
+    'items': [ 'head_0', 'body_0', 'legs_0' ]
+  },
+  {
+    'id': 'set_1',
+    'name': 'Hero of the Wild Set',
+    'items': [ 'head_1', 'body_1', 'legs_1' ]
+  },
+  {
+    'id': 'set_2',
+    'name': 'Zora Set',
+    'items': [ 'head_2', 'body_2', 'legs_2' ]
+  },
+  {
+    'id': 'set_3',
+    'name': 'Desert Voe Set',
+    'items': [ 'head_3', 'body_3', 'legs_3' ]
+  },
+  {
+    'id': 'set_4',
+    'name': 'Snowquill Set',
+    'items': [ 'head_4', 'body_4', 'legs_4' ]
+  },
+  {
+    'id': 'set_5',
+    'name': 'Flamebreaker Set',
+    'items': [ 'head_5', 'body_5', 'legs_5' ]
+  },
+  {
+    'id': 'set_6',
+    'name': 'Stealth Set',
+    'items': [ 'head_6', 'body_6', 'legs_6' ]
+  },
+  {
+    'id': 'set_7',
+    'name': 'Climber\'s Set',
+    'items': [ 'head_7', 'body_7', 'legs_7' ]
+  },
+  {
+    'id': 'set_8',
+    'name': 'Radiant Set',
+    'items': [ 'head_8', 'body_8', 'legs_8' ]
+  },
+  {
+    'id': 'set_9',
+    'name': 'Solider\'s Set',
+    'items': [ 'head_9', 'body_9', 'legs_9' ]
+  },
+  {
+    'id': 'set_10',
+    'name': 'Ancient Set',
+    'items': [ 'head_10', 'body_10', 'legs_10' ]
+  },
+  {
+    'id': 'set_11',
+    'name': 'Rubber Set',
+    'items': [ 'head_11', 'body_11', 'legs_11' ]
+  },
+  {
+    'id': 'set_12',
+    'name': 'Barbarian Set',
+    'items': [ 'head_12', 'body_12', 'legs_12' ]
+  },
+  {
+    'id': 'set_13',
+    'name': 'Gemstone Headgear',
+    'items': [ 'head_13', 'head_14', 'head_15', 'head_16', 'head_17', 'head_18' ]
+  },
+  {
+    'id': 'set_14',
+    'name': 'Others',
+    'items': [ 'body_13', 'head_19', 'head_20', 'head_21', 'head_22' ]
+  },
+]
