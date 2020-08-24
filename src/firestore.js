4e19987e9480c16219630e5530cbd2ed1b79fc1d
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-24 05:45:31 -0400

    Tidy up

--- src/firestore.js
+++ src/firestore.js
@@ -1,6 +1,14 @@
-import credentials from '../firebase-info.json';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseApp = firebase.initializeApp(credentials);
+const firebaseApp = firebase.initializeApp({
+  "apiKey": "AIzaSyDO-bUWgR9rU0E1kBEyQEPrPcaZSWZ7rQM",
+  "authDomain": "armor-tracker.firebaseapp.com",
+  "databaseURL": "https://armor-tracker.firebaseio.com",
+  "projectId": "armor-tracker",
+  "storageBucket": "armor-tracker.appspot.com",
+  "messagingSenderId": "101214793739",
+  "appId": "1:101214793739:web:18b4577ec75947d8f8970e"
+});
+
 export default firebaseApp.firestore();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <main id="home">
-    
+
   </main>
 </template>
 
 <script>
 import db from '@/firestore';
-import { armor, sets } from '@/assets/armor';
-import { items } from '@/assets/items';
+import armor from '@/assets/gamedata/armor.json';
+import items from '@/assets/gamedata/items.json';
+import sets from '@/assets/gamedata/sets.json';
 
 export default {
   name: 'Home',
@@ -15,14 +16,6 @@ export default {
     return {
       armor: [ ...armor ],
       items: [ ...items ],
-      user: {
-        progress: null
-      }
-    }
-  },
-  firestore: function() {
-    return {
-      'user.progress': db.collection('user-progress')
     }
   }
 }
