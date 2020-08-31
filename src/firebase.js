2776dfbf1f249334803b525056d9aab2b80dbb08
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-31 15:02:06 -0400

    Change Firebase project
    - Created a new project with cotera-io as the ID instead of
      armor-tracker

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,11 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 export default firebase.initializeApp({
-  "apiKey": "AIzaSyDO-bUWgR9rU0E1kBEyQEPrPcaZSWZ7rQM",
-  "authDomain": "armor-tracker.firebaseapp.com",
-  "databaseURL": "https://armor-tracker.firebaseio.com",
-  "projectId": "armor-tracker",
-  "storageBucket": "armor-tracker.appspot.com",
-  "messagingSenderId": "101214793739",
-  "appId": "1:101214793739:web:18b4577ec75947d8f8970e"
+  apiKey: "AIzaSyCAQWp46jkJDTJnfQ7L9HFVf3wKFdgnK6E",
+  authDomain: "cotera-io.firebaseapp.com",
+  databaseURL: "https://cotera-io.firebaseio.com",
+  projectId: "cotera-io",
+  storageBucket: "cotera-io.appspot.com",
+  messagingSenderId: "382557840417",
+  appId: "1:382557840417:web:79847d2b05fb4459de6f60"
 });
