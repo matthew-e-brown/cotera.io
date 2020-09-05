4415cd417ad72e639fc69319a5ba0fe4789fa734
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-09-05 04:29:20 -0400

    Create layout for updating password and email

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,14 +5,62 @@
       <h3>Email &amp; Password</h3>
       <template v-if="hasEmail">
         <div class="account-row">
-          <span>Email</span>
-          <span>{{ emailAddress }}</span>
+          <div>Email</div>
+          <div>{{ emailAddress }}</div>
           <button class="button">Change</button>
         </div>
-        <div class="account-row">
-          <span>Password</span>
-          <span>&ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;</span>
-          <button class="button">Change</button>
+        <div
+          class="account-row"
+          :class="{ update: password.state == 'update' }"
+        >
+          <div>Password</div>
+          <div v-if="password.state == 'initial'">
+            &ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;&ast;
+          </div>
+          <form v-else-if="password.state == 'update'">
+            <PasswordField
+              ref="password1"
+              id="old-password"
+              name="old-password"
+              placeholder="Current password"
+              autocomplete="current-password"
+              v-model="password.old"
+              @change="passwordToggle"
+            />
+            <PasswordField
+              ref="password2"
+              id="new-password-1"
+              name="new-password-1"
+              placeholder="New password"
+              autocomplete="new-password"
+              v-model="password.new1"
+              @change="passwordToggle"
+            />
+            <PasswordField
+              ref="password3"
+              id="new-password-2"
+              name="new-password-2"
+              placeholder="Re-type new password"
+              autocomplete="new-password"
+              v-model="password.new2"
+              @change="passwordToggle"
+            />
+            <div class="form-buttons">
+              <button
+                class="button"
+                @click="changePassword"
+              >Save</button>
+              <button
+                class="button"
+                @click="cancelPasswordChange"
+              >Cancel</button>
+            </div>
+          </form>
+          <button
+            v-if="password.state != 'update'"
+            class="button"
+            @click="password.state = 'update'"
+          >Change</button>
         </div>
       </template>
     </section>
@@ -26,11 +74,28 @@
 </template>
 
 <script>
+import PasswordField from '@/components/PasswordField.vue';
+
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export default {
   name: 'Account',
+  components: { PasswordField },
+  data: function() {
+    return {
+      email: {
+        state: 'initial',
+        newEmail: ''
+      },
+      password: {
+        state: 'initial',
+        old: '',
+        new1: '',
+        new2: ''
+      }
+    }
+  },
   computed: {
     hasEmail: function() {
       const { providerData } = firebase.auth().currentUser;
@@ -39,6 +104,22 @@ export default {
     emailAddress: function() {
       const { providerData } = firebase.auth().currentUser;
       return providerData.find(p => p.providerId == 'password').email;
+    }
+  },
+  methods: {
+    passwordToggle: function(value) {
+      this.$refs.password1.set(value);
+      this.$refs.password2.set(value);
+      this.$refs.password3.set(value);
+    },
+    cancelPasswordChange: function() {
+      this.password.state = 'initial';
+      this.password.old = '';
+      this.password.new1 = '';
+      this.password.new2 = '';
+    },
+    changePassword: function() {
+      return false;
     }
   }
 }
@@ -53,21 +134,6 @@ export default {
     padding-left: 3rem;
     padding-right: 3rem;
   }
-}
-
-.account-row {
-  margin: 0.85rem 0;
-  display: grid;
-  grid-template-columns: 2fr 5fr 2fr;
-  column-gap: 1em;
-  align-items: center;
-}
-
-.account-row>:nth-child(2) {
-  color: var(--body-text-1);
-  overflow-x: hidden;
-  word-wrap: none;
-  text-overflow: ellipsis;
 }
 
 h3 {
@@ -89,12 +155,63 @@ h3::after {
   background-color: var(--block-color-a);
 }
 
+.account-row {
+  margin: 0.85rem 0;
+  display: grid;
+  grid-template-columns: 7rem 2fr 1fr;
+  column-gap: 1em;
+  align-items: flex-start;
+}
+
+.account-row>div {
+  margin-top: 1.4em;
+}
+
+.account-row>:nth-child(2) {
+  color: var(--body-text-1);
+}
+
+.account-row:not(.update)>:nth-child(2) {
+  overflow-x: hidden;
+  word-wrap: none;
+  text-overflow: ellipsis;
+}
+
+.account-row input,
+.account-row >>> .hide-input {
+  margin-top: 0.4em;
+}
+
+.account-row input+input,
+.account-row >>> .hide-input+.hide-input {
+  margin-top: 0.85em;
+}
+
+.form-buttons {
+  display: flex;
+  justify-content: space-around;
+  flex-flow: row nowrap;
+  margin-top: 1em;
+}
+
+.form-buttons button {
+  margin: 0;
+}
+
 @media (max-width: 770px) {
   .account-row {
     grid-template-columns: repeat(3, 1fr);
     grid-template-areas:
       "h h h"
       "e e b";
+  }
+
+  .account-row.update {
+    row-gap: 0.85em;
+    grid-template-areas:
+      "h h h"
+      "e e e"
+      "b b b"
   }
 
   .account-row>*:nth-child(1) { grid-area: h; }
