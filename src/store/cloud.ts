4d655faf5d1e62976d3dfc49b8b213b956b8c273
Matthew Brown <matthew.e.brown.17@gmail.com>
2021-08-10 05:43:35 -0400

    Tweak build settings
    - Add Prerenderer plugin
    - Add Bundle Analyzer plugin
    - Fix lodash import in 'store/cloud.ts' to get proper tree-shaking

--- src/store/cloud.ts
+++ src/store/cloud.ts
@@ -1,4 +1,4 @@
-import { debounce } from 'lodash';
+import debounce from 'lodash/debounce';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,50 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const SPARenderer = PrerenderSPAPlugin.PuppeteerRenderer;
+const path = require('path');
+
 module.exports = {
   chainWebpack: config => {
     const svgRule = config.module.rule('svg');
 
     svgRule.uses.clear();
-
     svgRule
       .use('vue-loader')
       .loader('vue-loader-v16')
       .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader');
+
+    if (process.env.NODE_ENV == 'production') {
+      // Pre-render when building
+      config.plugin('prerender-spa-plugin').use(new PrerenderSPAPlugin({
+        staticDir: path.resolve(__dirname, 'dist'),
+        // Render everything that doesn't need auth
+        routes: [ '/', '/about', '/login', '/register' ],
+
+        // Minify the CSS and JS that are inserted into the page after load
+        minify: { minifyCSS: true, minifyJS: true, removeComments: true },
+
+        // Tell the renderer to wait for the 'main' element to appear; this is
+        // needed because the router needs a second to check if the user is
+        // signed out on '/login' and '/register'
+        renderer: new SPARenderer({ renderAfterElementExists: 'main' }),
+
+        // Tell Vue that this page is pre-rendered, and it just needs to be
+        // hydrated instead of fully repainted
+        postProcess: function(context) {
+          context.html = context.html
+            .replace('id="app"', '$& data-server-rendered="true"');
+          return context;
+        }
+      }));
+
+      // Generate a webpack stats file to be analyzed with 'npm run stats'
+      config.plugin('webpack-bundle-analyzer').use(new BundleAnalyzerPlugin({
+        analyzerMode: 'disabled',  // don't analyze it now...
+        generateStatsFile: true,   // ... just make the stats file
+      }))
+    }
   },
   configureWebpack: {
     module: {
